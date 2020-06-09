e59b770ae42b315988286ccffa73b5deace0bdd2
Stan S <stan.shm@gmail.com>
2020-06-08 23:29:11 -0500

    Updates

---
+++ precache-manifest.3ae4ae1e31be514f212f76e742ddc935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cf4abf8783b1fd088f181958006000",
+    "url": "/stansmemes/index.html"
+  },
+  {
+    "revision": "2732500d528394de53cd",
+    "url": "/stansmemes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0a84113ea09082d3fb04",
+    "url": "/stansmemes/static/css/main.6d3067ca.chunk.css"
+  },
+  {
+    "revision": "2732500d528394de53cd",
+    "url": "/stansmemes/static/js/2.c2895dbf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stansmemes/static/js/2.c2895dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a84113ea09082d3fb04",
+    "url": "/stansmemes/static/js/main.1d5c29ed.chunk.js"
+  },
+  {
+    "revision": "a7ed28ed71c720e18f98",
+    "url": "/stansmemes/static/js/runtime-main.d7799a9e.js"
+  }
+]);
